ef3aa39f293fbe9ae78c320f9b000e687a04ef86
xanderswillii <chedjouwilliam465@gmail.com>
2026-07-03 14:12:22 +0100

    created choppable small trees for the player

---
+++ scenes/components/collectible_component.gd
@@ -0,0 +1,9 @@
+class_name Collectible_component
+extends Area2D
+
+@export var collectible_name : String
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player: 
+		print("Collectted")
+		get_parent().queue_free()
