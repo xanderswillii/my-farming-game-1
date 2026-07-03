e0ef8730f5ea8417bf215aae6ab7cb9d7a64f3c1
xanderswillii <chedjouwilliam465@gmail.com>
2026-07-03 23:19:12 +0100

    made the large and small trees shake using a vertex shader

--- scenes/objects/trees/large_tree.gd
+++ scenes/objects/trees/large_tree.gd
@@ -8,11 +8,13 @@ var log_scene = preload("res://scenes/objects/trees/log.tscn")
 func _ready() -> void:
 	hurt_component.hurt.connect(on_hurt)
 	damage_component.max_damaged_reached.connect(on_max_damage_reached)
-	
-	
+
 func on_hurt(hit_damage: int) -> void:
 	damage_component.apply_damage(hit_damage)
-
+	material.set_shader_parameter("shake_intensity", 0.5)
+	await get_tree().create_timer(1.0).timeout
+	material.set_shader_parameter("shake_intensity", 0.0)
+	
 func on_max_damage_reached() -> void:
 	call_deferred("add_log_scene")
 	print("max damaged reached")
